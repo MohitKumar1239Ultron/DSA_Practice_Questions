87649e3874cc31e14781311e9f3db88b2068a745
Mohit Kumar <77378821+MohitKumar1239Ultron@users.noreply.github.com>
2022-01-13 12:53:20 +0530

    Add files via upload

---
+++ pat5.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i,j,n;
+	cin>>n;
+	for(i=1;i<=n;i++){
+		for(j=1;j<=n;j++){
+			if(i==j||j==1||i==n||j==n||i==j||i+j==n-1){
+				cout<<"*";
+			}
+			else{
+				cout<<" ";
+			}
+		}
+		cout<<"\n";
+	}	
+}
